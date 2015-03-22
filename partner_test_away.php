c7bdebb3a05b8fe3e6d9ed205aed32853a31bfdd
antonioortegajr <antonioortegajr@gmail.com>
2015-03-21 17:15:46 -0700

    removed echo email
    removed echo email

--- partner_test_away.php
+++ partner_test_away.php
@@ -6,11 +6,15 @@ $p_meth = array("listcomponents", "listmethods", "clients", "propertytypes", "ag
 
 require_once('api_call.php');
 
+$email_report ='The following issues were found in this test ' . $recreate_test_url . ' : ';
+
 foreach ($p_meth as $meth){
 
   $url = $test_url . $p_component . '/' . $meth;
   api_call_go('GET', '');
 
 }
+
+//echo '<hr><hr><hr><hr>' . $email_report;
 
 ?>
